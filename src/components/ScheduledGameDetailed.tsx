407fc43d69048c9c1c963e4537499dc56d103ed5
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-04-26 14:44:32 -0400

    feat: add team logo media + update fetch & Type definitions

--- src/components/ScheduledGameDetailed.tsx
+++ src/components/ScheduledGameDetailed.tsx
@@ -8,17 +8,22 @@ type ScheduledGameProps = {
 
 function ScheduledGameDetailed({ scheduledGameDataProp }: ScheduledGameProps) {
   return (
+    //#FIXME [APR 26] issue with keys
     <main id="component-container">
       <header className="pl-3">Upcoming Games</header>
       {scheduledGameDataProp.map((d) => (
-        <section className="flex pl-3">
-          {/* #FIXME: placeholder data. also need abbreviation data and logo data from api, team names too long */}
-          <div className="px-1">LOGO</div>
-          <div className="pr-1">NYY</div>
-          <div className="pr-1">@ LOGO</div>
-          <div className="pr-1">TOR</div>
-          <div className="pr-1">12:00</div>
-          <div> March 5</div>
+        <section>
+          <div key={d.gameID} className="flex pl-3">
+            {/* PLACEHOLDER DATA */}
+            {/* #FIXME: need abbreviation data, team names too long */}
+            <img src={d.awayTeamLogo} className="px-1 size-12"></img>
+            <div className="pr-1">{d.awayTeamName}</div>
+            <div>@</div>
+            <img src={d.homeTeamLogo} className="px-1 size-12"></img>
+            <div className="pr-1">{d.homeTeamName}</div>
+            <div className="pr-1">12:00</div>
+            <div> March 5</div>
+          </div>
         </section>
       ))}
     </main>
