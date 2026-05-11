0ff7d3c36aa71891d110f5819f8cf2d36fe4564a
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-05-11 11:28:39 -0400

    refactor: seperate concerns from RecentGame component to more reusable Game component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -14,30 +14,23 @@ type RecentGameProps = {
 
 function RecentGame({ recentGameDataProp }: RecentGameProps) {
   return (
-    <div
-      id="recent-game-scoreboard"
-      className="
-        flex justify-center 
-        p-30 
-        border-4
-      "
-    >
+    <>
       {recentGameDataProp.map((data) => (
         <div
           key={data.gameID}
           className="
-            p-10
+          p-3
             border-3
           "
         >
-          <div id="recent-game-date" className="text-center pb-4">
+          <div id="recent-game-date" className="text-center pb-6">
             <div>Date: {`${data.date}`}</div>
           </div>
           <div
-            id="recent-game-main-details"
+            id="game-main-details"
             className="
               flex justify-center
-              gap-25
+              gap-20
             "
           >
             <div>{data.awayTeamName}</div>
@@ -46,12 +39,12 @@ function RecentGame({ recentGameDataProp }: RecentGameProps) {
             <div>{data.homeTeamScore}</div>
             <div>{data.homeTeamName}</div>
           </div>
-          <div id="recent-game-additional-details" className="text-center">
+          <div id="game-venu" className="text-center">
             <div>{data.gameVenue}</div>
           </div>
         </div>
       ))}
-    </div>
+    </>
   );
 }
 
