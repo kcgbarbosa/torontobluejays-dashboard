6879f181a916d7d04f49bdf23f3db0024e66cafa
kc <kcgbarbosa@gmail.com>
2026-05-25 16:02:25 -0400

    feat: create roster table
    - Created Roster component that displays the Blue Jays player roster and their key details in a table format
    - Added the Roster component into RosterPage
    - Added loading and error state handling using existing Context
    - Implemented place of birth formatting

--- src/pages/RosterPage.tsx
+++ src/pages/RosterPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Roster from '../components/Roster';
 
 function RosterPage() {
   return (
     <div id="page-container" className="bg-gray-100 min-h-screen p-4">
-      Roster Page
+      <Roster />
     </div>
   );
 }
