7ca3c30a4ff9aa8b8fcb6c4dbee692cdfed27321
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-05-26 15:37:28 -0400

    feat: add modal skeleton to RosterPage

--- src/pages/RosterPage.tsx
+++ src/pages/RosterPage.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
 import Roster from '../components/Roster';
+import PlayerInfoModal from '../components/PlayerInfoModal';
+import { useState } from 'react';
 
 function RosterPage() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div id="page-container" className="bg-gray-100 min-h-screen p-4">
+      <button
+        className="border-2 rounded-2xl p-2"
+        onClick={() => setIsOpen(true)}
+      >Temp Button</button>
+      <PlayerInfoModal />
       <Roster />
     </div>
   );
