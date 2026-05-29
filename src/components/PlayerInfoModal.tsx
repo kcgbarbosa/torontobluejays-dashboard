f9a831171a3630881f1d0f08403d45d7ade0d3b0
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-05-29 13:40:55 -0400

    refactor: move modal out of Roster and update props

--- src/components/PlayerInfoModal.tsx
+++ src/components/PlayerInfoModal.tsx
@@ -2,36 +2,28 @@ import { useContext } from 'react';
 import { AppStatusContext, PlayerContext } from '../store/contexts';
 
 type PlayerID = {
-  playerIDProp: Number;
+  playerIDProp: number | null;
 };
 
+// # TODO-NEXT [May 26] Get Modal opening and closing
 function PlayerInfoModal({ playerIDProp }: PlayerID) {
+  const { isLoading, error } = useContext(AppStatusContext);
   const playerData = useContext(PlayerContext);
 
-  const { isLoading, error } = useContext(AppStatusContext);
+  const selectedPlayerData = playerData.find((d) => d.id === playerIDProp);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
-  const selectedPlayer = playerData.find(
-    (player) => player.id === playerIDProp
-  );
-
   return (
     <>
-      <span> Test Player Name is: {selectedPlayer?.fullName}</span>
-      <dialog id="playerModalID">
-        <div className=" w-11/12 max-w-5xl relative bg-blue-200 rounded-2xl shadow-2xl max-h-[80vh] overflow-y-auto">
+      <div id="playerModal">
+        <div className="overflow-y-auto">
           <button className="right-2 top-2">✕</button>
-          <h3 className="text-lg font-bold">Player Name</h3>
-          <p className="py-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo eum
-            suscipit amet rerum veritatis. Ullam mollitia, dolor ab itaque amet
-            alias a atque soluta ex asperiores cumque voluptas? Molestiae,
-            veritatis.
-          </p>
+          <h3 className="text-lg font-bold">{selectedPlayerData?.fullName}</h3>
+          <p className="py-4">Hits: {selectedPlayerData?.hitting?.hits}</p>
         </div>
-      </dialog>
+      </div>
     </>
   );
 }
--- src/pages/RosterPage.tsx
+++ src/pages/RosterPage.tsx
@@ -20,8 +20,7 @@ function RosterPage() {
   if (error) return <div>Error: {error}</div>;
   return (
     <div id="page-container" className="bg-gray-100 min-h-screen p-4">
-      {/* <PlayerInfoModal playerIDProp={} /> */}
-      <h1> ID {selectedPlayerID}</h1>
+      <PlayerInfoModal playerIDProp={selectedPlayerID} />
       <Roster onSelectPlayer={handleSelectPlayer} />
     </div>
   );
