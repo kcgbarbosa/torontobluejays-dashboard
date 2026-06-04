80a29bc021b8d18bca4f6f9b2150ec2afb5415e4
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-04 13:24:45 -0400

    style: update styling for FutureGameTableRow component

--- src/components/FutureGameTableRow.tsx
+++ src/components/FutureGameTableRow.tsx
@@ -4,23 +4,39 @@ import type { Game } from '../types/models/game.model';
 type GameProps = {
   gameData: Game;
 };
+
 function FutureGameTableRow({ gameData }: GameProps) {
+  const {
+    gameID,
+    date,
+    homeTeamName,
+    awayTeamName,
+    homeTeamLogo,
+    awayTeamLogo,
+    startTime,
+  } = gameData;
+
   return (
     <tr
-      key={gameData.gameID}
-      className="bg-white hover:bg-gray-100 transition-colors duration-200"
+      key={gameID}
+      className={`border-b border-slate-100 transition-colors duration-200 $`}
     >
-      <td className="px-4 py-4">{gameData.date}</td>
-      <td className="px-4 py-4">
-        <div className="flex align-left gap-2">
-          <img className="size-6" src={gameData.awayTeamLogo} />
-          <span>{gameData.awayTeamName}</span>
+      <td
+        className={`px-6 py-4 text-sm font-medium text-slate-500 whitespace-nowrap md:text-base`}
+      >
+        {date}
+      </td>
+
+      <td className="px-6 py-4 text-sm text-slate-900 lg:text-base">
+        <div className="flex items-center gap-3">
+          <img className="size-6 object-contain" src={awayTeamLogo} />
+          <span>{awayTeamName}</span>
           <p>@</p>
-          <img className="size-6" src={gameData.homeTeamLogo} />
-          <span>{gameData.homeTeamName}</span>
+          <img className="size-6" src={homeTeamLogo} />
+          <span>{homeTeamName}</span>
         </div>
       </td>
-      <td className="px-4 py-4">{formatTimeUtil(gameData.startTime)}</td>
+      <td className="px-4 py-4">{formatTimeUtil(startTime)}</td>
     </tr>
   );
 }
