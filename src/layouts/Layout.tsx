cd8578664e89fbd64e5365ae1e3d2dfa8476f06e
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-04 14:20:49 -0400

    fix: remove header from layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router-dom';
 
 function Layout() {
   return (
     <div>
-      <Header />
       <Navbar />
       <Outlet />
     </div>
