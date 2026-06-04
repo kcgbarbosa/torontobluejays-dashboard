304571736095158de763c02600e1cdc8f30d3623
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-04 16:02:39 -0400

    style: add gb to standings table with wip styling

--- src/components/ALEastStandings.tsx
+++ src/components/ALEastStandings.tsx
@@ -15,17 +15,30 @@ function ALEastStandings() {
   const alEastTeams = standingsData.filter((team) => team.divisionId === 201);
 
   return (
-    <div>
-      <h1 className="text-2xl">AL East Standings</h1>
-      {alEastTeams.map((d) => (
-        <div key={d.keyID}>
-          <>
-            <span>
-              {d.teamName}: {d.wins} - {d.losses}
-            </span>
-          </>
-        </div>
-      ))}
+    <div className="bg-white p-4 border rounded-2xl">
+      <h1 className="text-xl font-bold pb-6">AL East Standings</h1>
+      <div className="">
+        <table className="w-full border-collapse">
+          <thead>
+            <tr className="text-base font-bold border-b-2 border-slate-500 ">
+              <td className="pb-1">Team</td>
+              <td className="pb-1">W</td>
+              <td className="pb-1">L</td>
+              <td className="pb-1">GB</td>
+            </tr>
+          </thead>
+          <tbody>
+            {alEastTeams.map((team) => (
+              <tr>
+                <td>{team.teamName}</td>
+                <td>{team.wins}</td>
+                <td>{team.losses}</td>
+                <td>{team.gamesBack}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
