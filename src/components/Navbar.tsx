311038d97603193202cf2ed00d9548a1998da541
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-04 14:19:46 -0400

    refactor: combine header with navbar component styled

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,18 +8,24 @@ const navLinkInfo = [
 
 function Navbar() {
   return (
-    <nav className="flex justify-around py-4  ">
-      {navLinkInfo.map((d) => (
-        <Link key={d.id}
-          to={d.href}
-          className="
-          text-black no-underline 
-              hover:underline hover:text-blue-300"
-        >
-          {d.name}
-        </Link>
-      ))}
-    </nav>
+    <div className="flex w-full justify-between items-center ">
+      <h1 className=" text-3xl font-bold text-blue-400 pl-10 pb-5  font-medium">
+        Blue Jays Dashboard
+      </h1>
+      <nav className="flex items-center gap-10 text-xl pb-5 pr-10   ">
+        {navLinkInfo.map((d) => (
+          <Link
+            key={d.id}
+            to={d.href}
+            className="
+        text-black no-underline 
+        hover:underline hover:text-blue-300"
+          >
+            {d.name}
+          </Link>
+        ))}
+      </nav>
+    </div>
   );
 }
 
