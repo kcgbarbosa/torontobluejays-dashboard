7c38bb728d0845bb0c23b78e9806d22235c0c3e8
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-06 12:09:24 -0400

    feat: implement homerunleader card on homepage

--- src/components/StatCard.tsx
+++ src/components/StatCard.tsx
@@ -6,21 +6,24 @@
  */
 
 import { useContext } from 'react';
-import { AppStatusContext } from '../store/contexts';
+import { AppStatusContext, PlayerContext } from '../store/contexts';
+import { getStatLeader } from '../utils/statisticUtilities';
 
 type StatCardProps = {
-  title: string;
-  name: string;
-  value: number | string;
-  jerseyNumber?: number;
+  statName: string | undefined;
+  playerName: string | undefined;
+  playerID: number | undefined;
+  statValue: number | undefined;
+  jerseyNumber?: string | undefined;
   positionAbbreviation?: string;
   statAbbreviation?: string;
 };
 
 function StatCard({
-  title,
-  name,
-  value,
+  statName,
+  playerName,
+  playerID,
+  statValue,
   jerseyNumber,
   positionAbbreviation,
   statAbbreviation,
@@ -34,31 +37,26 @@ function StatCard({
     if (error) return <div> Error : {error} </div>;
   }
 
-  // #TODO [June 5] create statisticUtilities.ts and below functions
-  // const hitLeaderID = // insert returned value from future getHitLeaderUtil
-  // const homeRunLeaderID = // insert returned value from future getHomeRunLeaderUtil
-  // const opsLeaderID = // insert returned value from future getOPSLeaderUtil
-
-  // const statID =
-
   return (
     <div className="bg-white border border-gray-200 rounded-xl shadow-sm p-4">
-      <h2 className="text-base font-semibold text-gray-900 pb-4">{}</h2>
+      <h2 className="text-base font-semibold text-gray-900 pb-4">{statName}</h2>
       <div className="flex items-center gap-4">
         <img
-          src={`https://midfield.mlbstatic.com/v1/people/${statID}/spots/120`}
+          src={`https://midfield.mlbstatic.com/v1/people/${playerID}/spots/120`}
           className="size-20 rounded-lg object-contain bg-gray-50"
         />
         <div className="flex flex-col gap-1">
-          <span className="text-sm font-semibold text-gray-900">
-            {name}
-            <span className="text-blue-600">#{jerseyNumber}</span>
+          <span className="text-sm font-xwsemibold text-gray-900">
+            {playerName}
+            <span className="text-blue-600"> #{jerseyNumber}</span>
           </span>
           <span className="text-xs text-gray-500 uppercase tracking-wider">
             {positionAbbreviation}
           </span>
           <div className="flex items-baseline gap-1 mt-2">
-            <span className="text-2xl font-bold text-gray-900">{value}</span>
+            <span className="text-2xl font-bold text-gray-900">
+              {statValue}
+            </span>
             <span className="text-xs text-gray-400 uppercase tracking-wider">
               {statAbbreviation}
             </span>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,10 +9,23 @@ import ALEastStandings from '../components/ALEastStandings';
 import StatCard from '../components/StatCard';
 import PastGameResultsCard from '../components/PastGameResultsCard';
 import { useContext } from 'react';
-import { HeroGameContext } from '../store/contexts';
+import { HeroGameContext, PlayerContext } from '../store/contexts';
+import { getStatLeader } from '../utils/statisticUtilities';
 
 function HomePage() {
   const heroGame = useContext(HeroGameContext);
+  const playerData = useContext(PlayerContext);
+
+  const homeRunLeader = getStatLeader(
+    playerData,
+    (d) => d.hitting?.homeRuns ?? -1
+  );
+
+  const hitsLeader = getStatLeader(playerData, (d) => d.hitting?.hits ?? -1);
+
+  const opsLeader = getStatLeader(playerData, (d) =>
+    parseFloat(d.hitting?.ops ?? '-1')
+  );
 
   return (
     <div id="page-container" className="bg-gray-50 min-h-screen w-full p-4">
@@ -31,7 +44,15 @@ function HomePage() {
             <ALEastStandings />
           </div>
           <div>
-            <StatCard />
+            <StatCard
+              statName="Home Run Leader"
+              playerName={homeRunLeader?.fullName}
+              playerID={homeRunLeader?.id}
+              statValue={homeRunLeader?.hitting?.homeRuns}
+              jerseyNumber={homeRunLeader?.jerseyNumber}
+              positionAbbreviation={homeRunLeader?.positionAbbreviation}
+              statAbbreviation="HR"
+            />
           </div>
         </section>
       </main>
