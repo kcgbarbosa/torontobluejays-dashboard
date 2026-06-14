c8eca140a942994def8e27d527a2391cdfce1fc9
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-14 13:40:02 -0400

    test: getStatLeader for homeruns - multiple player path

---
+++ src/utils/statisticUtilities.test.ts
@@ -0,0 +1,35 @@
+import type { Player } from '../types/models/person.model';
+import { getStatLeader } from './statisticUtilities';
+
+describe('retrieve leader of given statistic', () => {
+  it('retrieve home run leader from array of multiple players', () => {
+    const playerData = [
+      {
+        firstName: 'MrWinner',
+        hitting: {
+          homeRuns: 999,
+        },
+      },
+      {
+        firstName: 'MissInBetweener',
+        hitting: {
+          homeRuns: 10,
+        },
+      },
+      {
+        firstName: 'SirLoser',
+        hitting: {
+          homeRuns: 1,
+        },
+      },
+    ] as Player[];
+    expect(getStatLeader(playerData, (d) => d.hitting?.homeRuns ?? -1)).toEqual(
+      {
+        firstName: 'MrWinner',
+        hitting: {
+          homeRuns: 999,
+        },
+      }
+    );
+  });
+});
