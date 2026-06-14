1ba764a2e5ab329d70ce898155f294cd4e49beec
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-14 11:47:38 -0400

    test: add isGameInPast test - future game path

--- src/utils/dateAndTimeUtilities.test.ts
+++ src/utils/dateAndTimeUtilities.test.ts
@@ -2,7 +2,7 @@ import type { Game } from '../types/models/game.model';
 import { isGameInPast } from './dateAndTimeUtilities';
 
 // #TODO NEXT [June 13] Write tests for the other paths
-describe.only('isGameInPast', () => {
+describe('isGameInPast', () => {
   it('returns true for a game in the past ', () => {
     // #NOTE [June 13] define game object which contains only the required isGameInPast arguments
     const game = {
@@ -10,5 +10,12 @@ describe.only('isGameInPast', () => {
       startTime: '2024-01-01T18:00:00Z',
     } as Game; // #NOTE [June 13] 'as Game' to make TS happy. the arg im passing is Game' shaped i swear <3
     expect(isGameInPast(game)).toBe(true);
+  });
+  it('returns false if game is not in past', () => {
+    const game = {
+      date: '2027-01-01',
+      startTime: '2024-01-01T18:00:00Z',
+    } as Game;
+    expect(isGameInPast(game)).toBe(false);
   });
 });
