960850c6dce19995b3181e7bedbb42eea991cbda
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-15 13:27:37 -0400

    feat: create useMLBData and extract fetchAllData from DataProvider

---
+++ src/hooks/useMLBData.ts
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import type { ALRecords } from '../types/models/standings.model';
+import type { Game, Season } from '../types/models/game.model';
+import type { Player } from '../types/models/person.model';
+import {
+  fetchALTeamRecords,
+  fetchHeroGameData,
+  fetchRosterData,
+  fetchSchedule,
+  fetchSeasonData,
+} from '../services/apiService';
+
+export function useMLBData() {
+  const [standingsData, setStandingsData] = useState<ALRecords[]>([]);
+  const [seasonData, setSeasonData] = useState<Season[]>([]);
+  const [scheduleData, setScheduleData] = useState<Game[]>([]);
+  const [heroGameData, setHeroGameData] = useState<Game | null>(null);
+  const [playerData, setPlayerData] = useState<Player[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchAllData = async () => {
+      try {
+        setIsLoading(true);
+
+        const season = await fetchSeasonData();
+        setSeasonData(season);
+
+        const schedule = await fetchSchedule(season);
+        setScheduleData(schedule);
+
+        const heroGame = await fetchHeroGameData(schedule);
+        setHeroGameData(heroGame);
+
+        const standings = await fetchALTeamRecords();
+        setStandingsData(standings);
+
+        const players = await fetchRosterData();
+        setPlayerData(players);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : String(err));
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchAllData();
+  }, []);
+  return {
+    standingsData,
+    seasonData,
+    scheduleData,
+    heroGameData,
+    playerData,
+    isLoading,
+    error,
+  };
+}
