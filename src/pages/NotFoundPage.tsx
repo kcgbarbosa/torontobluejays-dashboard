05c72feb447c35e338281403d20f3504dcc64806
Kevin-Christian Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-20 14:37:46 -0400

    feat: update notfoundpage
    - Created /assets/fonts and assets/imgs folders
    - Implemented NotFoundPage featuring custom styled "Page not found" messaging.
    - Added a "Back to Home" navigation route using the Link component.
    - Included an image asset of my dog wearing a Blue Jays hat.

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NotFoundPage() {
   return (
-    <div>
-      <h1>Oops, No page found!</h1>
+    <div className="grid grid-cols-1 min-h-11/12 md:grid-cols-[auto_auto] bg-white gap-4 justify-center items-center p-10">
+      <div>
+        <div className="text-6xl text-gray-900 tracking-tight font-semibold pb-4 ">
+          Page Not Found...
+        </div>
+        <div className="flex gap-2 pt-2 text-xl text-gray-900 ">
+          Kingsley will escort you back:
+          <Link
+            to={'/'}
+            className="text-xl text-blue-300 hover:text-blue-600 underline"
+          >
+            Home
+          </Link>
+        </div>
+      </div>
+      <img
+        className="size-100 rounded-2xl"
+        src="src/assets/imgs/dPMMCb_GR_iM-cCO_FtCgw.webp"
+      />
     </div>
   );
 }
