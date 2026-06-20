943e80dabe1ce53ce207e4936c422f50533e71bb
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-20 15:13:30 -0400

    wip: layout layer loading spinner

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import { useContext } from 'react';
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router-dom';
+import { AppStatusContext } from '../store/contexts';
+import { RingLoader } from 'react-spinners';
 
 function Layout() {
+  const appStatus = useContext(AppStatusContext);
+  const { isLoading } = appStatus;
+
   return (
-    <div>
+    <div className="flex flex-col min-h-screen ">
       <Navbar />
-      <Outlet />
+      {isLoading ? (
+        <div className="flex flex-1 items-center justify-center">
+          <RingLoader size={120} />
+        </div>
+      ) : (
+        <Outlet />
+      )}
     </div>
   );
 }
