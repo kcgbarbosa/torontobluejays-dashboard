23681eddb7e44fd0a195bc943654f6852a5f0220
Kevin-Christian Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-22 19:25:02 -0400

    feat: navbar hamburger menu
    - Installed `react-icons` for the `RxHamburgerMenu` icon
    - Added hamburger menu toggle that renders a dropdown on small screens 
    - Replaced text logo with Blue Jays logo image 
    - Made navbar sticky

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+import { RxHamburgerMenu } from 'react-icons/rx';
 import { Link, NavLink } from 'react-router-dom';
+import logoURL from '../assets/imgs/logo-bluejays.png';
 
 const navLinkInfo = [
   { id: 1, name: 'Home', href: '/' },
@@ -7,23 +10,49 @@ const navLinkInfo = [
 ];
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleClick = () => setIsOpen(!isOpen);
+
   return (
-    <nav className="flex w-full justify-between border-b border-gray-300 bg-blue-50 px-10 py-5">
-      <Link
-        to={'/'}
-        className="text-3xl text-blue-600 font-semibold tracking-tighter"
-      >
-        Toronto Blue Jays 
+    <nav className="sticky top-0 z-10 flex w-full justify-between border-b border-gray-300 bg-blue-200 px-10 py-5">
+      <Link to={'/'}>
+        <img src={logoURL} className="w-36 lg:w-48" />
       </Link>
-      <div className="flex items-center gap-8">
+      <div className="flex flex-col items-center md:hidden">
+        <button onClick={handleClick} className="">
+          <RxHamburgerMenu />
+        </button>
+        {isOpen && (
+          <div className="absolute border text-lg font-semibold tracking-wide rounded-lg top-full flex flex-col w-36 gap-1 right-0 bg-white/80 z-10">
+            {navLinkInfo.map((d) => (
+              <NavLink
+                key={d.id}
+                to={d.href}
+                className={({ isActive }) =>
+                  `rounded-md px-1 py-2 text-center text-base font-medium transition-colors duration-200 ${
+                    isActive
+                      ? 'text-blue-600 font-semibold'
+                      : 'text-gray-600 hover:text-blue-400'
+                  }`
+                }
+                onClick={() => setIsOpen(false)}
+              >
+                {d.name}
+              </NavLink>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="hidden md:flex gap-4">
         {navLinkInfo.map((d) => (
           <NavLink
             key={d.id}
             to={d.href}
             className={({ isActive }) =>
-              `py-2 text-base font-medium transition-colors duration-200 ${
+              `py-4 text-center text-base font-medium transition-colors duration-200 ${
                 isActive
-                  ? 'text-blue-600 font-semibold border-b-2 border-blue-600'
+                  ? 'text-blue-600 font-semibold border-b-3 border-blue-600'
                   : 'text-gray-600 hover:text-blue-400'
               }`
             }
