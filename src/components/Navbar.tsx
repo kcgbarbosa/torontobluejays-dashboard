3587c86c1770df4795dc0bfd5e50205750ba09e3
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-22 16:11:18 -0400

    feat: add hamburger menu icon and handleClick

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { RxHamburgerMenu } from 'react-icons/rx';
 import { Link, NavLink } from 'react-router-dom';
 
 const navLinkInfo = [
@@ -7,27 +9,35 @@ const navLinkInfo = [
 ];
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleClick = () => setIsOpen(!isOpen);
+
   return (
     <nav className="flex w-full justify-between border-b border-gray-300 bg-blue-50 px-10 py-5">
       <Link to={'/'}>
         <img src="src/assets/imgs/logo-bluejays.png" className="w-36 lg:w-48" />
       </Link>
-      <div className="flex items-center gap-8">
-        {navLinkInfo.map((d) => (
-          <NavLink
-            key={d.id}
-            to={d.href}
-            className={({ isActive }) =>
-              `py-2 text-base font-medium transition-colors duration-200 ${
-                isActive
-                  ? 'text-blue-600 font-semibold border-b-2 border-blue-600'
-                  : 'text-gray-600 hover:text-blue-400'
-              }`
-            }
-          >
-            {d.name}
-          </NavLink>
-        ))}
+      <div className="flex flex-col items-center gap-8">
+        <button onClick={handleClick}>
+          <RxHamburgerMenu />
+        </button>
+        {isOpen &&
+          navLinkInfo.map((d) => (
+            <NavLink
+              key={d.id}
+              to={d.href}
+              className={({ isActive }) =>
+                `py-2 text-base font-medium transition-colors duration-200 ${
+                  isActive
+                    ? 'text-blue-600 font-semibold border-b-2 border-blue-600'
+                    : 'text-gray-600 hover:text-blue-400'
+                }`
+              }
+            >
+              {d.name}
+            </NavLink>
+          ))}
       </div>
     </nav>
   );
