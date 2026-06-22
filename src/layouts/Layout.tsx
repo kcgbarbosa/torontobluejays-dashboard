76682f0a1e61f33ee8a6d1820ed05ff8c6276444
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-06-22 12:36:59 -0400

    feat: add circleloader and error messaging

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,18 +2,22 @@ import { useContext } from 'react';
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router-dom';
 import { AppStatusContext } from '../store/contexts';
-import { RingLoader } from 'react-spinners';
+import { CircleLoader } from 'react-spinners';
 
 function Layout() {
   const appStatus = useContext(AppStatusContext);
-  const { isLoading } = appStatus;
+  const { isLoading, error } = appStatus;
 
   return (
     <div className="flex flex-col min-h-screen ">
       <Navbar />
       {isLoading ? (
         <div className="flex flex-1 items-center justify-center">
-          <RingLoader size={120} />
+          <CircleLoader />
+        </div>
+      ) : error ? (
+        <div className="flex flex-1 items-center justify-center">
+          <p>Something went wrong. Please refresh.</p>
         </div>
       ) : (
         <Outlet />
